b83c1ef4a21608c17b397293c4f96b805c33ca56
Kenny GUIOUGOU <kenny.guiougou@gmail.com>
2016-09-23 16:35:25 +0200

    corrections Insight

--- src/AppBundle/Controller/StaticController.php
+++ src/AppBundle/Controller/StaticController.php
@@ -3,11 +3,6 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use AppBundle\Services\LoLAPIService;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class StaticController extends Controller
 {
