2b4b27e4b44d973ddaedf62a320b2c73f9ee0846
adrien <adrien.caselles@gmail.com>
2016-10-16 18:08:48 +0200

    Ajout des données sur les pages

---
+++ src/AppBundle/DataFixtures/ORM/LoadPageData.php
@@ -0,0 +1,62 @@
+<?php
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\Page;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadPageData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{      
+        /**
+        * @var ContainerInterface
+        */
+        private $container;
+       
+         /**
+        * {@inheritDoc}
+        */
+        public function setContainer(ContainerInterface $container = null)
+        {
+            $this->container = $container;
+        }
+        
+        
+	public function load(ObjectManager $manager)
+	{
+            $faker = \Faker\Factory::create();
+            
+            $userManager = $this->container->get('fos_user.user_manager');
+            
+            //Create Page Home
+            $page = new Page();
+            $page->setEnglishTest("[ENGLISH]" . $faker->text(400));
+            $page->setFrenchText("[FRANCAIS]" . $faker->text(400));
+            $page->setName("HOME");
+            $page->setUser($this->getReference('user'.rand(0, 9)));
+            $page->setLastUpdateDate(new \DateTime());
+            $this->addReference('pagehome', $page);
+            $manager->persist($page);
+            
+            //Create Page Contact
+            $pageContact = new Page();
+            $pageContact->setEnglishTest("[ENGLISH]" . $faker->text(400));
+            $pageContact->setFrenchText("[FRANCAIS]" . $faker->text(400));
+            $pageContact->setName("CONTACT");
+            $pageContact->setUser($this->getReference('user'.rand(0, 9)));
+            $pageContact->setLastUpdateDate(new \DateTime());
+            $this->addReference('pagecontact', $pageContact);
+            $manager->persist($pageContact);
+            
+            //Flush Data
+            $manager->flush();
+	}
+	
+	public function getOrder()
+	{
+		return 4;
+	}
+}
