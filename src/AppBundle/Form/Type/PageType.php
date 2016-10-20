3d4287d2a53f6c4f5741c4ca93c330add76fec30
adrien <adrien.caselles@gmail.com>
2016-10-20 23:50:13 +0200

    Ajout du type de formulaire : Page

---
+++ src/AppBundle/Form/Type/PageType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PageType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')
+                ->add('french_text', 'textarea')
+                ->add('english_text', 'textarea');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Page'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_page';
+    }
+
+
+}
