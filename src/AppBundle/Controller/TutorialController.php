645e6e61761159e3a0ed10923a20e965261f1503
adrien <adrien.caselles@gmail.com>
2016-12-11 16:07:37 +0100

    Ajoute de l'affichage des tuto (admin + public)

--- src/AppBundle/Controller/TutorialController.php
+++ src/AppBundle/Controller/TutorialController.php
@@ -23,6 +23,22 @@ class TutorialController extends Controller
     
     public function showAction($id){
         
+        $em = $this->getDoctrine()->getManager();
+        $tutorial = $em->getRepository('AppBundle:Tutorial')->findOneById($id);
+        
+        // tutorial exist
+        if (!$tutorial) {
+            throw $this->createNotFoundException(
+                    '[Tutorial] No found for id ' . $id
+            );
+        }
+        
+        $lang = $this->get('request')->getLocale();
+        
+        return $this->render('AppBundle:Tutorial:tutorial.show.html.twig', array(
+            'tutorial' => $tutorial,
+            'lang'      => $lang
+        ));
     } 
     
     public function createAction(){
