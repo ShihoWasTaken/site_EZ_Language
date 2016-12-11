23282c23b5f9db8d0b3d3ec62aa5c32c636161bd
adrien <adrien.caselles@gmail.com>
2016-12-11 10:53:21 +0100

    Unused use statement should be avoided

--- src/AppBundle/Controller/StaticController.php
+++ src/AppBundle/Controller/StaticController.php
@@ -5,7 +5,6 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Contact;
 use AppBundle\Form\Type\ContactType;
-use Symfony\Component\HttpFoundation\Response;
 
 class StaticController extends Controller
 {
