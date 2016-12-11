1566683729ffb384c670e1558193c0f7b057a2c4
adrien <adrien.caselles@gmail.com>
2016-12-11 16:40:19 +0100

    Edition d'un tutorial

--- src/AppBundle/Controller/TutorialController.php
+++ src/AppBundle/Controller/TutorialController.php
@@ -69,5 +69,35 @@ class TutorialController extends Controller
     
     public function editAction($id){
         
+        //Get Function by Id
+        $em         = $this->getDoctrine()->getManager();
+        $tutorial   = $em->getRepository('AppBundle:Tutorial')->findOneById($id);
+
+        // Function exist
+        if (!$tutorial) {
+            throw $this->createNotFoundException(
+                    '[Tutorial] No found for id ' . $id
+            );
+        }
+
+        //Create form
+        $form = $this->get('form.factory')->create(new TutorialType, $tutorial, array(
+            'locale' => $this->get('request')->getLocale())
+        );
+        
+        $request = $this->getRequest();
+        $form->handleRequest($request);
+        
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($tutorial);
+
+            $em->flush();
+            return $this->redirectToRoute('app_admin_tutorialList');
+        }
+
+        return $this->render('AppBundle:Tutorial:tutorial.edit.html.twig', array(
+                'form' => $form->createView()
+        ));
     } 
 }
