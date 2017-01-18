ea1f75ac645464a9aab3a352b3d63e766f115de3
Phobie53 <chevenzo@hotmail.fr>
2017-01-18 09:37:01 +0100

    Ajout de CKEditor bindé au champ

--- src/AppBundle/Resources/public/ckeditor/build-config.js
+++ src/AppBundle/Resources/public/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/d7ae58d1d29644615a9ad62d37a0dccc
+ * (2) http://ckeditor.com/builder/e69bef4171a1ba149506bf9fcb56231a
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/d7ae58d1d29644615a9ad62d37a0dccc
+ * (3) http://ckeditor.com/builder/download/e69bef4171a1ba149506bf9fcb56231a
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -26,7 +26,7 @@
 
 var CKBUILDER_CONFIG = {
 	skin: 'moono-lisa',
-	preset: 'full',
+	preset: 'standard',
 	ignore: [
 		'.bender',
 		'bender.js',
@@ -53,71 +53,40 @@ var CKBUILDER_CONFIG = {
 		'a11yhelp' : 1,
 		'about' : 1,
 		'basicstyles' : 1,
-		'bidi' : 1,
 		'blockquote' : 1,
 		'clipboard' : 1,
-		'colorbutton' : 1,
-		'colordialog' : 1,
+		'codesnippet' : 1,
 		'contextmenu' : 1,
-		'copyformatting' : 1,
-		'dialogadvtab' : 1,
-		'div' : 1,
 		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
 		'filebrowser' : 1,
-		'find' : 1,
-		'flash' : 1,
 		'floatingspace' : 1,
-		'font' : 1,
 		'format' : 1,
-		'forms' : 1,
 		'horizontalrule' : 1,
-		'htmlbuttons' : 1,
 		'htmlwriter' : 1,
-		'iframe' : 1,
 		'image' : 1,
-		'indentblock' : 1,
 		'indentlist' : 1,
-		'insertpre' : 1,
-		'justify' : 1,
-		'language' : 1,
 		'link' : 1,
 		'list' : 1,
-		'liststyle' : 1,
 		'magicline' : 1,
 		'maximize' : 1,
-		'newpage' : 1,
-		'pagebreak' : 1,
 		'pastefromword' : 1,
 		'pastetext' : 1,
-		'preview' : 1,
-		'print' : 1,
 		'removeformat' : 1,
 		'resize' : 1,
-		'save' : 1,
 		'scayt' : 1,
-		'selectall' : 1,
-		'showblocks' : 1,
 		'showborders' : 1,
-		'smiley' : 1,
 		'sourcearea' : 1,
 		'specialchar' : 1,
 		'stylescombo' : 1,
-		'stylesheetparser' : 1,
 		'tab' : 1,
 		'table' : 1,
 		'tabletools' : 1,
-		'templates' : 1,
-		'texttransform' : 1,
 		'toolbar' : 1,
-		'uicolor' : 1,
 		'undo' : 1,
-		'videodetector' : 1,
 		'wsc' : 1,
-		'wysiwygarea' : 1,
-		'xml' : 1,
-		'youtube' : 1
+		'wysiwygarea' : 1
 	},
 	languages : {
 		'en' : 1,
