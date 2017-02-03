7124e11f748cb5ff248f7a473b966c184cc8d80f
adrien3 <adrien.caselles@gmail.com>
2017-02-03 10:01:14 +0100

    Création formulaire pour la création d'un article

---
+++ src/AppBundle/Form/Type/ArticleBlogType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class ArticleBlogType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('french_title')
+                ->add('english_title')
+                ->add('french_html')
+                ->add('english_html');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\ArticleBlog'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_article_blog';
+    }
+
+
+}
