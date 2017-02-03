877802dddcdaf3f5e3b9e3971f425b1a249236d4
adrien3 <adrien.caselles@gmail.com>
2017-02-03 10:30:22 +0100

    Partie création d'un article

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -65,5 +65,27 @@ class BlogController extends Controller
         return $this->render('AppBundle:Blog:articleBlog.edit.html.twig', array(
                 'form' => $form->createView()
         ));
+    }
+
+    public function createArticleAction(){
+        $articleBlog = new ArticleBlog();
+
+        //Create form
+        $form = $this->get('form.factory')->create(new ArticleBlogType, $articleBlog);
+        
+        $request = $this->getRequest();
+        $form->handleRequest($request);
+        
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($articleBlog);
+
+            $em->flush();
+            return $this->redirectToRoute('app_admin_articleBlogList');
+        }
+
+        return $this->render('AppBundle:Blog:articleBlog.create.html.twig', array(
+                'form' => $form->createView()
+        ));
     } 
 }
